6590464aed0faaae94277feb4dc825d71a82f458
DmitriyKovchugo <dkovchugo@mail.ru>
2022-05-16 12:42:13 +0300

    fix: img preloader

--- portfolio/js/imagesPreloader.js
+++ portfolio/js/imagesPreloader.js
@@ -2,7 +2,7 @@ export const imagesPreloader = (images) => {
    images.forEach((el) => {
       for(let i = 1; i <= 6; i++) {
          const img = new Image();
-         img.src = `../img/${el}/${i}.jpg`;
+         img.src = `./../img/${el}/${i}.jpg`;
       }
    })
 }
